0cfbf56628ef8dbb614965466f8c757c2f9d08ae
aviroopjana <aviroopjana@gmail.com>
2024-03-04 04:57:34 +0530

    feat: add homepage component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,41 @@
-import backgroundBanner from '../../public/assets/background-banner.jpg'
+import { Input } from "@/components/ui/input";
+import backgroundBanner from "../../public/assets/background-banner.jpg";
+import { Button } from "@/components/ui/button";
 
 const Homepage = () => {
   return (
-    <div>
-      <div className="w-full">
-        <img src={backgroundBanner} alt="background" className="object-cover h-[750px] w-full"/>
+    <div className="relative">
+      <img
+        src={backgroundBanner}
+        alt="background"
+        className="object-cover h-[750px] w-full"
+      />
+      <div className="absolute inset-0">
+        <div className="max-w-5xl flex flex-col mx-auto text-white md:mt-28">
+          <p className="text-xl font-bold">Welcome to EatsXpress!!</p>
+          <h1 className="mt-4 text-4xl md:text-5xl md:max-w-lg font-bold">
+            The Fastest Healthy Food Delivery In{" "}
+            <span className="text-rose-950">Your City</span>
+          </h1>
+          <p className="md:max-w-md mt-4 font-medium">
+            Our mission at EatsXpress is to Delivering fresh, healthy meals with
+            lightning speed. Enjoy a diverse menu for breakfast, lunch, and
+            dinner, crafted for busy individuals and families.
+          </p>
+          <div className="flex flex-row items-center  mt-4">
+            <Input
+              type="tel"
+              placeholder="Enter your phone number"
+              className="md:max-w-xs rounded-3xl h-10 text-white placeholder-[#4c0519] bg-transparent border focus:ring-[#4c0519] border-[#4c0519]"
+            />
+            <Button className="bg-[#4c0519] hover:bg-[#93080b] h-10 rounded-3xl transition-transform duration-300">
+              Add to Cart
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
