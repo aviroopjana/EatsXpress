391961211d74f21c7625612a20291ed98f4db259
aviroopjana <aviroopjana@gmail.com>
2024-03-09 23:25:16 +0530

    created userSlice and added to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {userReducer},
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
---
+++ src/redux/user/userSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
+
+export interface userState {
+  currentUser: currentUserType | null;
+  error: string | null;
+  loading: boolean;
+}
+
+export interface currentUserType {
+  _id: string;
+  username: string;
+  email: string;
+  name: string;
+  profilePicture: string;
+  phone: string;
+  address: string;
+  city: string;
+  pincode: string;
+  accountType: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const initialState: userState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.error = null;
+      state.loading = true;
+    },
+    signInSuccess: (state, action: PayloadAction<currentUserType>) => {
+      state.currentUser = action.payload;
+      state.error = null;
+      state.loading = false;
+    },
+    signInFailure: (state, action: PayloadAction<string>) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
+export default userSlice.reducer;
