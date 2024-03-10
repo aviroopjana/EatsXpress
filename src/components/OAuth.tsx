049f992b9891f7d8b48f1f6e7e27896bb726134f
aviroopjana <aviroopjana@gmail.com>
2024-03-10 20:49:30 +0530

    feat: Implement complete oauth functionality

--- src/components/OAuth.tsx
+++ src/components/OAuth.tsx
@@ -1,12 +1,49 @@
 import { FcGoogle } from "react-icons/fc"
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+import { app } from "@/firebase";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { signInSuccess } from "@/redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 const OAuth = () => {
   const path = useLocation().pathname;
 
+  const auth = getAuth(app);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleGoogleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const resultsFromGoogle = await signInWithPopup(auth, provider);
+
+      const res = await fetch('/api/auth/google', {
+        method: "POST",
+        headers: {
+          'Content-type' : "application/json",
+        },
+        body: JSON.stringify({
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          googlePhotoUrl: resultsFromGoogle.user.photoURL
+        }),
+      });
+
+      const data = await res.json();
+      if(res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
   return (
-    <Button type="button" className="w-full">
+    <Button type="button" className="w-full" onClick={handleGoogleClick}>
       <FcGoogle className="h-6 w-6 mr-2"/>
       <span className="text-[15px]">
         {path === '/sign-up' ? 'Sign Up with Google' : 'Sign In with Google'}
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqHD2eMVGnEWnXCJXdNHzC7FNdVlT3jCI",
+  authDomain: "eatsxpress-6299d.firebaseapp.com",
+  projectId: "eatsxpress-6299d",
+  storageBucket: "eatsxpress-6299d.appspot.com",
+  messagingSenderId: "363083543280",
+  appId: "1:363083543280:web:97148e63cea8883cb04835",
+  measurementId: "G-3K0201HN0Y"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
