4b3eda44ab3a83304b48f1fa2fd00813f13a46bc
aviroopjana <aviroopjana@gmail.com>
2024-03-18 00:10:18 +0530

    Improved the sign up schema

--- src/schemas/SignupSchema.ts
+++ src/schemas/SignupSchema.ts
@@ -1,30 +1,34 @@
 import * as z from 'zod';
 
+const accountTypes = ["personal", "family", "business"];
+
 const SignupSchema = z.object({
-  name: z.string().min(1, {
+  name: z.string().min(3, {
     message: "Name is required",
   }),
-  username: z.string().min(1, {
+  username: z.string().min(3, {
     message: "Username is required",
   }),
   email: z.string().email({
-    message: "Invalid email format",
+    message: "Please provide a valid email address",
   }),
-  password: z.string().min(6, {
-    message: "Password must be at least 6 characters long",
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
   }),
-  accountType: z.enum(["personal", "family", "business"]),
+  accountType: z.string().refine(value => accountTypes.includes(value), {
+    message: "Selecting an account type is mandatory"
+  }),
   phone: z.string().min(10, {
     message: "Phone number is required",
   }),
-  address: z.string().min(1, {
+  address: z.string().min(5, {
     message: "Address is required",
   }),
-  city: z.string().min(1, {
+  city: z.string().min(3, {
     message: "City is required",
   }),
   pincode: z.string().min(4, {
-    message: "Pincode is required",
+    message: "valid pincode is required",
   }),
 });
 
