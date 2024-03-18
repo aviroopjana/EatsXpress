9b6a6d7a6c6fdfaf4abf52cdc2aef4588ef5a638
aviroopjana <aviroopjana@gmail.com>
2024-03-18 23:38:26 +0530

    feat: add ManageRestaurant component with conditional rendering for CreateRestaurant and UpdateRestaurant

--- src/components/ManageRestaurant.tsx
+++ src/components/ManageRestaurant.tsx
@@ -1,7 +1,22 @@
+import { RootState } from "@/redux/store"
+import { useSelector } from "react-redux"
+import CreateRestaurant from "./CreateRestaurant";
+import UpdateRestaurant from "./UpdateRestaurant";
+
 const ManageRestaurant = () => {
+
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
   return (
-    <div>ManageRestaurant</div>
-  )
-}
+    <div className="h-[950px] inset-0 flex justify-center items-center top-0 z-[-2] w-screen bg-[radial-gradient(#4c0519_1px,#fdd752_1px)] bg-[size:20px_20px]">
+        <div className="max-w-4xl w-full p-8 mx-auto backdrop-filter backdrop-blur-md rounded-2xl shadow-xl py-10 overflow-hidden border-t-4 border-t-red-950"> 
+        <h1 className="text-3xl font-semibold text-center ">
+          Manage Your Restaurant
+        </h1>
+        {currentUser?.restaurantId === null ? <CreateRestaurant/> : <UpdateRestaurant/>}
+        </div>
+    </div>
+  );
+};
 
 export default ManageRestaurant
