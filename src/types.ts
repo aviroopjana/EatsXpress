e867dfdb4cf58badc09047014574b43bc08adb78
aviroopjana <aviroopjana@gmail.com>
2024-04-01 02:03:12 +0530

    feat: add types for MenuItem, Restaurant, and RestaurantSearchResponse in a separate section

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export type MenuItem = {
+  productId: string;
+  name: string;
+  description: string;
+  price: number;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Restaurant = {
+  _id: string;
+  restaurantName: string;
+  location: string;
+  owner: string;
+  estimatedDeliveryTime: number;
+  deliveryPrice: number;
+  imageUrl: string;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type RestaurantSearchResponse = {
+  data: Restaurant[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
+  };
+};
