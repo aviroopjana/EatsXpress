7a2013dd03ee8f835eeb5d9d8883eb38b1a62035
aviroopjana <aviroopjana@gmail.com>
2024-04-01 02:02:29 +0530

    feat: add and setup react-query for complex data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,23 @@ import { persistor, store } from "./redux/store.ts";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <PersistGate persistor={persistor}>
-    <Provider store={store}>
-      <App />
-      <Toaster position="top-right" richColors />
-    </Provider>
-  </PersistGate>
+  <QueryClientProvider client={queryClient}>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+        <Toaster position="top-right" richColors />
+      </Provider>
+    </PersistGate>
+  </QueryClientProvider>
 );
