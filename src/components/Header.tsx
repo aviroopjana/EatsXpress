b2747c7971c3056c7d5d649e947881afaebbedfb
aviroopjana <aviroopjana@gmail.com>
2024-04-07 21:21:22 +0530

    refactor header component to display the active state of nav links and cart item count

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 import { CiSearch } from "react-icons/ci";
 import { BsCart3 } from "react-icons/bs";
@@ -15,6 +15,12 @@ const Header = () => {
 
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const isCartDetailPage = location.pathname.startsWith("/cartdetail");
+
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
@@ -26,6 +32,10 @@ const Header = () => {
     { id: "4", title: "Delivery", path: "/delivery" },
     { id: "5", title: "Contact", path: "/contact" },
   ];
+
+  const { items } = useSelector((state: RootState) => state.cart);
+
+  const cartItemCount = items.length;
 
   return (
     <div className="bg-gradient-to-r from-[#fed754] to-[#edbd41] ">
@@ -40,13 +50,17 @@ const Header = () => {
           </div>
           <div className="hidden sm:flex flex-row gap-6">
             {navLinks.map((navLink) => (
-              <Link
+              <NavLink
                 to={navLink.path}
                 key={navLink.id}
-                className="text-base font-semibold text-gray-600 transition-colors duration-300 hover:text-rose-950"
+                className={({ isActive }) =>
+                  `text-base font-semibold text-gray-500 transition-colors duration-300 hover:text-rose-950 ${
+                    isActive ? "text-rose-950 font-bold text-lg" : ""
+                  }`
+                }
               >
                 {navLink.title}
-              </Link>
+              </NavLink>
             ))}
           </div>
           <div className="hidden sm:flex flex-row gap-2">
@@ -54,12 +68,31 @@ const Header = () => {
               <Button variant={"ghost"} className="hover:bg-transparent">
                 <CiSearch size={25} />
               </Button>
-              <Button variant={"ghost"} className="hover:bg-transparent">
-                <BsCart3 size={25} />
+              <Button
+                variant={"ghost"}
+                className="hover:bg-transparent"
+                onClick={() =>
+                  navigate(`/cartdetail/${currentUser?.restaurantId}`)
+                }
+              >
+                <div className="relative">
+                  <BsCart3
+                    size={isCartDetailPage ? 30 : 25}
+                    color={isCartDetailPage ? "brown" : "black"}
+                  />
+                  {cartItemCount > 0 && (
+                    <span className="absolute top-0 right-0 bg-red-500 text-white text-sm rounded-full w-4 h-4 flex items-center justify-center">
+                      {cartItemCount}
+                    </span>
+                  )}
+                </div>
               </Button>
             </div>
             {currentUser ? (
-              <UserDropdownMenu isOpen={isDropdownOpen} toggleDropdown={toggleDropdown} />
+              <UserDropdownMenu
+                isOpen={isDropdownOpen}
+                toggleDropdown={toggleDropdown}
+              />
             ) : (
               <Button
                 variant={"ghost"}
